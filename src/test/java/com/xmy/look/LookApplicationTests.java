dcc4f177b3dc02a207a77515f526c18f741384c0
X-mak <2310828236@qq.com>
2022-01-04 10:21:54 +0800

    初始模板

---
+++ src/test/java/com/xmy/look/LookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmy.look;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
